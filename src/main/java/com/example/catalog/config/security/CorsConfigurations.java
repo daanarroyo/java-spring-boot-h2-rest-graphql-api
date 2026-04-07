ce85fae5770e03dd30e955ecb419a8f386651b08
danielarroyo <daanrroyob@gmail.com>
2026-04-07 08:31:33 -0600

    Adding Cors Configuration

---
+++ src/main/java/com/example/catalog/config/security/CorsConfigurations.java
@@ -0,0 +1,17 @@
+package com.example.catalog.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurations implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
+}
